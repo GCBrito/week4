58b3610575bd51bb28c18aed38073aad6b442a56
Guilherme Camargo Brito <gctbrito@gmail.com>
2018-01-12 20:25:23 -0200

    updated column names for assigment 3

--- rankall.R
+++ rankall.R
@@ -3,7 +3,7 @@ rankall <- function(outcome, num = "best") {
         # 1. load data and produce a data.frame with useful columns 
                 data <- read.csv("outcome-of-care-measures.csv", sep=",", na.strings="Not Available", stringsAsFactors=FALSE)
                 mydata <- data[,c(2,7,11,17,23)]
-                colnames(mydata) <- c("Hospital.Name","State","heart attack","heart failure","pneumonia")
+                colnames(mydata) <- c("hospital","state","heart attack","heart failure","pneumonia")
         
         # 2. Check whether outcome is valid
                 validout<- outcome %in% c("pneumonia","heart attack", "heart failure")
@@ -21,15 +21,15 @@ rankall <- function(outcome, num = "best") {
         
         # 4. Creates a factor with levels for every state, a data frame for storing results, and a vector with states abbreviations
                 mydata6 <- as.data.frame(matrix(NA,ncol=2,nrow=54))
-                colnames(mydata6) <- c("Hospital.name","State")
-                statesn <- factor(mydata2$State)
+                colnames(mydata6) <- c("hospital","state")
+                statesn <- factor(mydata2$state)
                 stsn<- levels(statesn)
         
         # 5. Order data by state, then by outcome ranking and finally alphabetically  
                 mydata3 <-  mydata2[order(mydata2[,2],mydata2[,outcome],mydata2[,1]),]
         
         # 6. Split data according to state
-                mydata4 <- split(mydata3,mydata3$State)
+                mydata4 <- split(mydata3,mydata3$state)
                 
         # 7. Produce a data.frame with the requested rank hospital which has a row for each hospital in that ranking for each state    
         if      (class(num)=="numeric"){
