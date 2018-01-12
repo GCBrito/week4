7f751f4f64ad52a59087da46e9f4093e7e2afed7
Guilherme Camargo Brito <gctbrito@gmail.com>
2018-01-12 18:28:24 -0200

    Final R programming assignment

--- rankall.R
+++ rankall.R
@@ -1,41 +1,56 @@
 rankall <- function(outcome, num = "best") {
+      
         # 1. load data and produce a data.frame with useful columns 
-        data <- read.csv("outcome-of-care-measures.csv", sep=",", 
-                         na.strings="Not Available", stringsAsFactors=FALSE)
-        
-        mydata <- data[,c(2,7,11,17,23)]
-        colnames(mydata) <- c("Hospital.Name","State","heart attack" ,
-                              "heart failure","pneumonia")    
+                data <- read.csv("outcome-of-care-measures.csv", sep=",", na.strings="Not Available", stringsAsFactors=FALSE)
+                mydata <- data[,c(2,7,11,17,23)]
+                colnames(mydata) <- c("Hospital.Name","State","heart attack","heart failure","pneumonia")
         
         # 2. Check whether outcome is valid
-        validout<- outcome %in% c("pneumonia","heart attack", "heart failure")
-        if(validout==0){stop("invalid outcome")}
+                validout<- outcome %in% c("pneumonia","heart attack", "heart failure")
+                if(validout==0){stop("invalid outcome")}
         
-        # 3. Creates a factor with levels for every state and a data frame for storing results
+        # 3. Remove hospitals with NA in outcome 
+        if      (as.character(outcome)=="heart attack"){
+                mydata2 <- mydata[complete.cases(mydata$'heart attack'),] }
         
-        st <- as.factor(mydata$State)
-        df <- matrix(,nrow=0,ncol=0)
-    
-        # 4. 
-         
-         
-        if (class(num)=="numeric"){ if(num>nrow(mydata3)) {return(NA)}
-                else {  x <- function(){ mydata4 <-  mydata[order(mydata[,outcome],mydata[,1]),]  }
-                return(mydata4$Hospital.Name[num])}
-        }
-      
-        else if (num=="best") {   
-                mydata4 <- mydata[which.min(mydata[,outcome]),]
-                print(mydata4$Hospital.Name) }
+        else if (as.character(outcome)=="heart failure"){
+                mydata2 <- mydata[complete.cases(mydata$'heart failure'),] }
         
-       
+        else  
+                mydata2 <- mydata[complete.cases(mydata$'pneumonia'),] 
+        
+        # 4. Creates a factor with levels for every state, a data frame for storing results, and a vector with states abbreviations
+                mydata6 <- as.data.frame(matrix(NA,ncol=2,nrow=54))
+                colnames(mydata6) <- c("Hospital.name","State")
+                statesn <- factor(mydata2$State)
+                stsn<- levels(statesn)
+        
+        # 5. Order data by state, then by outcome ranking and finally alphabetically  
+                mydata3 <-  mydata2[order(mydata2[,2],mydata2[,outcome],mydata2[,1]),]
+        
+        # 6. Split data according to state
+                mydata4 <- split(mydata3,mydata3$State)
+                
+        # 7. Produce a data.frame with the requested rank hospital which has a row for each hospital in that ranking for each state    
+        if      (class(num)=="numeric"){
+                for(i in 1:54) {   mm<- data.frame(mydata4[i])
+        
+                          mydata6[i,] <- mm[num,c(1)]}}
+        
+        else if (num=="best") {
+                for(i in 1:54) {   mm<- data.frame(mydata4[i])
+        
+                          mydata6[i,] <- mm[1,c(1)]}}
+                
         else if (num=="worst") {
-                mydata4 <- mydata[which.max(mydata[,outcome]),]
-                print(mydata4$Hospital.Name) } 
-        
-        
-        
-        ## For each state, find the hospital of the given rank
-        ## Return a data frame with the hospital names and the
-        ## (abbreviated) state name
+                for(i in 1:54) {   mm<- data.frame(mydata4[i])
+                
+                          mydata6[i,] <- mm[nrow(mm),c(1)]}}
+                
+        # 8. Assigns names to the data.frame rows and then returns the final data.frame
+                
+        rownames(mydata6) <- stsn
+        mydata6[2] <- stsn 
+        mydata6
+       
 }
